d211de609f968614ddd242bb475bcef6df049cfd
Dobromir Minchev <dobri87@gmail.com>
2015-07-01 23:50:37 +0300

    Song upload form

--- app/models/category.rb
+++ app/models/category.rb
@@ -8,4 +8,12 @@ class Category < ActiveRecord::Base
   friendly_id :name, use: :slugged
 
   enum parent: [ :electronic_music, :cdm, :caribbean_music, :other ]
+
+  def self.group_parents
+    groups = []
+    Category.parents.keys.each do |parent|
+      groups << [ parent.humanize.upcase, Category.send(parent).order('name asc').map { |c| [c.name, c.id]}]
+    end
+    groups
+  end
 end
