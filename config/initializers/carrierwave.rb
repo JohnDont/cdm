0ce06d9425ea50c0426e4bc1e288c894ee5d5f53
Dobromir Minchev <dobri87@gmail.com>
2015-07-19 20:03:16 +0300

    Update rackspace details

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,12 +5,12 @@ CarrierWave.configure do |config|
 
     config.fog_credentials = {
       :provider           => 'Rackspace',
-      :rackspace_username => 'cdmgeneration',
-      :rackspace_api_key  => 'c230660445ab4019aee077ad844d6ece',
+      :rackspace_username => 'cdmgeneration2',
+      :rackspace_api_key  => '3e3366ecd4174e768223f1e8a4896bdb',
       :rackspace_region   => 'dfw'
     }
     config.fog_directory = "cdm-production"
-    config.asset_host = "http://74f83991cf514c4e6663-dc57254a24758b4af0852933688cfe33.r43.cf1.rackcdn.com"
+    config.asset_host = "http://1d0a8b4c250cc24b5eb9-a3cb6292aa3b047007f9ae237bc150e3.r83.cf1.rackcdn.com"
   else
     config.storage = :file
     config.asset_host = ActionController::Base.asset_host
